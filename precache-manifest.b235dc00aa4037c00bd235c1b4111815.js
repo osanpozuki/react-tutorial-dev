2ec3fcc023ecf7f4a121a9529d75f5388701c249
tama <tama.osanpozuki@gmail.com>
2019-02-23 12:41:32 +0900

    Updates

---
+++ precache-manifest.b235dc00aa4037c00bd235c1b4111815.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b2d15347dbf2dd6be24",
+    "url": "/react-tutorial/static/js/runtime~main.9b2d1534.js"
+  },
+  {
+    "revision": "eabbc75b2a6d9176465a",
+    "url": "/react-tutorial/static/js/main.eabbc75b.chunk.js"
+  },
+  {
+    "revision": "170d78dc7eaf86966fd3",
+    "url": "/react-tutorial/static/js/2.170d78dc.chunk.js"
+  },
+  {
+    "revision": "eabbc75b2a6d9176465a",
+    "url": "/react-tutorial/static/css/main.1cd00ab2.chunk.css"
+  },
+  {
+    "revision": "ad345da6681615ad78f66d17af3e23b8",
+    "url": "/react-tutorial/index.html"
+  }
+];
